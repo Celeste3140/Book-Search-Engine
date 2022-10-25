fb1dd9a4921a389fa2f7d353a54cc481dd39e65a
carambola <celestebaltazar@yahoo.com>
2022-10-24 20:01:44 -0700

    the resolvers

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,5 @@
+const { User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
+
+module.exports = resolvers
