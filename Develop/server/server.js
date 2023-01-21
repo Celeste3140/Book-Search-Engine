1c36cf5ac594837981c009aeb8c70d0d179a9399
carambola <celestebaltazar@yahoo.com>
2023-01-20 18:54:29 -0800

    added to test gql api

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -32,5 +32,13 @@ app.get('*', (req, res) => {
 });
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+
+  });
+});
+
+process.on('uncaughtException', function(err) {
+  console.log('Caught exception: ' + err);
 });
